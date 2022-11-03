e730a05761e6c463e2461409df8f3bc5a5c82f9d
João Vitor Soares Rocha <115377157+JoViSoRo@users.noreply.github.com>
2022-11-03 13:22:09 -0300

    Baus fechando e abrindo

---
+++ bausumiu.js
@@ -0,0 +1,41 @@
+var bauAparecer = document.querySelector(".bau1");
+
+bauAparecer.addEventListener("click", function () {
+  var bau = document.querySelector(".bau1");
+  bau.classList.toggle("hide");
+  var bau = document.querySelector(".bau1-b");
+  bau.classList.toggle("hide");
+});
+
+var bauAparecer2 = document.querySelector(".bau2");
+bauAparecer2.addEventListener("click", function () {
+  var bau = document.querySelector(".bau2");
+  bau.classList.toggle("hide");
+  var bau = document.querySelector(".bau2-b");
+  bau.classList.toggle("hide");
+});
+
+var bauAparecer3 = document.querySelector(".bau3");
+bauAparecer3.addEventListener("click", function () {
+  var bau = document.querySelector(".bau3");
+  bau.classList.toggle("hide");
+  var bau = document.querySelector(".bau3-b");
+  bau.classList.toggle("hide");
+});
+
+var bauAparecer4 = document.querySelector(".bau4");
+bauAparecer4.addEventListener("click", function () {
+  var bau = document.querySelector(".bau4");
+  bau.classList.toggle("hide");
+  var bau = document.querySelector(".bau4-b");
+  bau.classList.toggle("hide");
+});
+
+var bauAparecer5 = document.querySelector(".bau5");
+bauAparecer5.addEventListener("click", function () {
+  var bau = document.querySelector(".bau5");
+  bau.classList.toggle("hide");
+  var bau = document.querySelector(".bau5-b");
+  bau.classList.toggle("hide");
+});
+t
